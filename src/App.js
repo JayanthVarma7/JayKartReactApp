52d11ff1f2824f1adf9e76f670ff0076e7978010
JayanthVarma7 <konduru.jayanthvarma@gmail.com>
2023-08-01 14:58:57 +0530

    added renderon link to connect backend

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks";
 
 export const config = {
-  endpoint: `https://jaykartfrontend.herokuapp.com/api/v1`,
+  endpoint: `https://jaykart.onrender.com/api/v1`,
 };
 
 
